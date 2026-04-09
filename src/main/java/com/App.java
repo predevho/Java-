882232c19cd519ff3092140d179aff6a1d0d2334
predevho <dlagusgh7894@gmail.com>
2026-04-09 15:03:59 +0900

    명언앱 refactor

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -4,15 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-//scanner 객체를 이용해서 터미널 입력 받기
 public class App {
     Scanner scanner = new Scanner(System.in);
-    //while문안에 넣으면 끝날때 초기화가 되므로 밖에다가 선언
     int lastId = 0;
-    List<WiseSaying> wiseSayings = new ArrayList<>();
-    //메서드를 세분화하기 위해서 필드값으로 올림
+    List<WiseSaying> wiseSayingList = new ArrayList<>();
 
-    void run() {
+    void run () {
         System.out.println("== 명언 앱 ==");
 
         while (true) {
@@ -22,7 +19,7 @@ public class App {
             if (cmd.equals("종료")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
-            } else if (cmd.equals("등록")) {
+            } else  if (cmd.equals("등록")) {
                 actionWrite();
             } else if (cmd.equals("목록")) {
                 actionList();
@@ -36,23 +33,21 @@ public class App {
     }
 
     void actionWrite() {
-        System.out.print("명언 : ");
-        String Content = scanner.nextLine().trim();
-        //메서드 체이닝
+        System.out.print("명언: ");
+        String content = scanner.nextLine().trim();
 
-        System.out.print("작가 : ");
-        String Author = scanner.nextLine().trim();
+        System.out.print("작가: ");
+        String author = scanner.nextLine().trim();
 
-        WiseSaying wiseSaying = write(Author, Content);
+        WiseSaying wiseSaying = write(author, content);
 
-        System.out.printf("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
-
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId()));
     }
 
-    WiseSaying write(String author, String content) {
-        WiseSaying wiseSaying = new WiseSaying(++lastId, author, content);
+    WiseSaying write (String author, String content) {
+        WiseSaying wiseSaying = new WiseSaying(++lastId, author, content );
 
-        wiseSayings.add(wiseSaying);
+        wiseSayingList.add(wiseSaying);
 
         return wiseSaying;
     }
@@ -60,79 +55,77 @@ public class App {
     void actionList() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
-        //for문을 이용해서 wiseSayings 리스트에 있는 요소들을 역순으로 출력
-        //-1은 인덱스의 크기보다 -1만큼 작기때문에
-        for (int i = wiseSayings.size() - 1; i >= 0; i--) {
-            WiseSaying wiseSaying = wiseSayings.get(i);
-            {
-                System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
-            }
-        }
 
+        for (int i = wiseSayingList.size() - 1; i >=0; i--) {
+            WiseSaying wiseSaying = wiseSayingList.get(i);
+            System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
+        }
     }
 
     void actionDelete(String cmd) {
         String[] cmdBits = cmd.split("=");
 
-        if (cmdBits.length < 2 || cmdBits[1].isEmpty()) {
-            System.out.println(" id를 입력해주세요");
+        if (cmdBits.length < 2 ||  cmdBits[1].isEmpty()) {
+            System.out.println("id를 입력해주세요.");
             return;
         }
+
         int id = Integer.parseInt(cmdBits[1]);
 
-        delete(id);
+        WiseSaying wiseSaying = findById(id);
+
+        delete(wiseSaying);
 
         System.out.println("%d번 명언이 삭제되었습니다.".formatted(id));
     }
 
-    void delete(int id) {
-        WiseSaying wiseSaying = null;
-        for (int i = 0; i < wiseSayings.size(); i++) {
-            if (wiseSayings.get(i).getId() == id) {
-                wiseSaying = wiseSayings.get(i);
-            }
-        }
-        if (wiseSaying == null) {
-            System.out.println("해당 아이디는 존재하지 않습니다.");
-            return;
-        }
-
-        wiseSayings.remove(wiseSaying);
+    void delete(WiseSaying wiseSaying) {
+        wiseSayingList.remove(wiseSaying);
     }
 
     void actionModify(String cmd) {
         String[] cmdBits = cmd.split("=");
 
-        if (cmdBits.length < 2 || cmdBits[1].isEmpty()) {
-            System.out.println(" id를 입력해주세요");
+        if (cmdBits.length < 2 ||  cmdBits[1].isEmpty()) {
+            System.out.println("id를 입력해주세요.");
             return;
         }
+
         int id = Integer.parseInt(cmdBits[1]);
 
-        modify(id);
+        WiseSaying wiseSaying = findById(id);
 
-        System.out.println("%d번 명언이 수정되었습니다.".formatted(id));
+        System.out.printf("명언(기존) : %s\n",  wiseSaying.getContent());
+        System.out.print("명언 : ");
+        String content = scanner.nextLine().trim();
+
+        System.out.printf("작가(기존) : %s\n",  wiseSaying.getAuthor());
+        System.out.print("작가 : ");
+        String author = scanner.nextLine().trim();
+
+        modify(wiseSaying, content, author);
+
+        System.out.println("%d번 명언이 수정 되었습니다.".formatted(id));
     }
 
-    void modify(int id){
+    void modify(WiseSaying wiseSaying, String content, String author) {
+        wiseSaying.setContent(content);
+        wiseSaying.setAuthor(author);
+    }
+
+    WiseSaying findById(int id) {
         WiseSaying wiseSaying = null;
-        for(int i = 0; i < wiseSayings.size(); i++) {
-            if (wiseSayings.get(i).getId() == id) {
-                wiseSaying = wiseSayings.get(i);
+        for (int i = 0; i < wiseSayingList.size(); i++) {
+            if (wiseSayingList.get(i).getId() == id) {
+                wiseSaying = wiseSayingList.get(i);
             }
         }
+
         if (wiseSaying == null) {
-            System.out.println("%d번 아이디는 존재하지 않습니다.".formatted(id));
+            System.out.println("해당 아이디는 존재하지 않습니다.");
+            return null;
         }
 
-        System.out.println("명언(기존): %s".formatted(wiseSaying.getContent()));
-        System.out.print("명언: ");
-        String content = scanner.nextLine().trim();
-        wiseSaying.setContent(content);
-
-        System.out.println("작가(기존): %s".formatted(wiseSaying.getAuthor()));
-        System.out.print("작가: ");
-        String author = scanner.nextLine().trim();
-        wiseSaying.setAuthor(author);
+        return wiseSaying;
     }
 }
