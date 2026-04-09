86ec006cbcd1dcca2186b00bdd01bf122e6ed6dd
predevho <dlagusgh7894@gmail.com>
2026-04-09 09:51:09 +0900

    명언앱 4단계

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,32 +1,36 @@
 package com;
 
 import java.util.Scanner;
+
 //scanner 객체를 이용해서 터미널 입력 받기
 public class Main {
     static void main() {
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
 
+        int lastId = 0;
+
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
 
-            if (cmd.equals("종료")){
+            if (cmd.equals("종료")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
-            }
-            else if (cmd.equals("등록")){
+            } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                String wiseSayingContent= scanner.nextLine().trim();
+                String wiseSayingContent = scanner.nextLine().trim();
                 //메서드 체이닝
 
                 System.out.print("작가 : ");
-                String wiseSayingAuthor= scanner.nextLine().trim();
+                String wiseSayingAuthor = scanner.nextLine().trim();
 
-                System.out.print("1번 명언이 등록되었습니다.");
+                int number = ++lastId;
+
+                System.out.printf("%d번 명언이 등록되었습니다.",(number));
             }
         }
 
-    scanner.close();
+        scanner.close();
     }
 }
